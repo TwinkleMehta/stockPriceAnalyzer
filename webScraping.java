e6eae9e398c3f40bc5bfafef7aa54393ac2581c4
TwinkleMehta <twinklemehta8@gmail.com>
2017-03-21 18:00:49 -0400

    Updated webScraping
    Added comments

--- webScraping.java
+++ webScraping.java
@@ -5,11 +5,53 @@ import java.util.*;
 
 public class webScraping {
 	
+	public static void main(String[]args) throws IOException
+	{
+		//Adds all stock symbols to array list stockSymbols
+		ArrayList<String> stockSymbols = new ArrayList<String>();
+		stockSymbols = getStockSymbols(); 
+		
+		for(int i=0;i<stockSymbols.size();i++)
+		{
+
+			//finds real time price of stock
+			String price = findPrice(getUrlSource(stockSymbols.get(i)));
+			
+			//outputs symbol and price in terminal
+			System.out.println("Symbol: "+ stockSymbols.get(i));
+			System.out.print(" Price: " + price);
+			
+			//writing hourly prices of stock to hourlyPrice.txt
+			File textFile; 
+			FileWriter out; 
+			try 
+			{
+				textFile = new File("/Users/MehtaUser/Documents/EclipseProjects2016/Stocks/hourlyPrices.txt"); 
+				out = new FileWriter(textFile, true); 
+				BufferedWriter writeFile = new BufferedWriter(out);
+				 
+				writeFile.write(price + " ");
+				
+				writeFile.close();
+				out.close();
+			}
+			
+			catch(IOException e)
+			{
+				System.out.println("Problem Writing to File");
+		        System.err.println("IOException: "+e.getMessage());
+			}
+			
+		}
+		
+	}
+	
 	public static ArrayList <String> getStockSymbols()
 	{
-		
+		//new array list of all stock symbols
 		ArrayList<String> stockSymbols = new ArrayList<String>();
 		
+		//reads symbols.txt to retrieve all stock symbols to be analyzed
 		File textFile = new File("/Users/MehtaUser/Documents/EclipseProjects2016/Stocks/symbols.txt");
 		FileReader in; 
 		BufferedReader readFile; 
@@ -38,45 +80,6 @@ public class webScraping {
 		}
 		
 		return(stockSymbols);
-		
-	}
-	
-	public static void main(String[]args) throws IOException
-	{
-		//Get all stocks to check 
-		ArrayList<String> stockSymbols = new ArrayList<String>();
-		stockSymbols = getStockSymbols(); 
-		
-		for(int i=0;i<stockSymbols.size();i++)
-		{
-			System.out.println("Symbol: "+ stockSymbols.get(i)); 
-			String price = findPrice(getUrlSource(stockSymbols.get(i)));	
-			
-			System.out.println("Price: " + price);
-			
-			File textFile; 
-			FileWriter out; 
-			
-			try 
-			{
-				textFile = new File("/Users/MehtaUser/Documents/EclipseProjects2016/Stocks/hourlyPrices.txt"); 
-				out = new FileWriter(textFile, true); 
-				BufferedWriter writeFile = new BufferedWriter(out);
-				 
-				writeFile.write(price + " ");
-				
-				writeFile.close();
-				out.close();
-			}
-			
-			catch(IOException e)
-			{
-				System.out.println("Problem Writing to File");
-		        System.err.println("IOException: "+e.getMessage());
-			}
-			
-		}
-		
 		
 	}
 	
@@ -109,26 +112,26 @@ public class webScraping {
 	}
 	
 	private static String getUrlSource(String url) throws IOException {
-        String line = " ";
+        
+		//accessing source code
+		String line = " ";
 		URL website = new URL("http://www.nasdaq.com/symbol/"+url);
         URLConnection yc = website.openConnection();
         BufferedReader in = new BufferedReader(new InputStreamReader(
                 yc.getInputStream(), "UTF-8"));
         String inputLine;
-
+        
+        //searching source code for "LastSale"
         while ((inputLine = in.readLine()) != null)
         {
-        	
         	if(inputLine.contains("LastSale"))
         	{
         		line = inputLine; 
         		break; 
         	}
-        	
         }
             
         in.close();
-
         return line;
     }
 
